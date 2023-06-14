bbd97085b621b50e72f0076b8c728d16807a9f8b
+ <+>
2023-06-14 15:59:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun (activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun()
+
+//Monday work function
+function mondayWork (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+}
+mondayWork()
+
+///wrapAdjective Function
+function wrapAdjective (string){
+   return function () {
+    if (string == '*') {
+        return `You are ${string}a hard worker${string}!`
+        }
+    
+    else if (string === '||'){
+        return `You are ${string}a dedicated programmer${string}!`
+    }
+    
+
+    }
+}
